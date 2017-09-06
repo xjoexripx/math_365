e834abc974e8dc79e988e2e8f442294902229ca2
Joe Rippee <joe@swymhome.com>
2017-09-06 13:02:11 -0600

    Update HW_2.m

--- HW1/HW_2.m
+++ HW1/HW_2.m
@@ -1,49 +1,45 @@
-%%This script factors a number into its prime factors and prints results.
-in = input('Please input an integer that is greater than 1: ');
-factors = my_factor(in);
-print_factors(factors, in);
+clear;
 
-function print_factors(factors, n)
-    if isempty(factors)
-        fprintf('%d is a prime number\n', n)
-    else
-        fprintf('%d = %d', n, factors(1))
-        for i = 2:numel(factors)
-            fprintf(' * %d', factors(i))
-        end
-        fprintf('\n')
-    end
-end
+user_input = input('Please enter a positive integer above 1: ', 's'); 
+number_being_tested = str2num(user_input); %string 2 number
+list_of_prime_numbers = [2,3,5,7,11,13,17,19,23,29,31,37];
+original_number = number_being_tested;
 
-function factors = my_factor(n)
-    %handle 2 as input
-    if n == 2
-        factors = [];
-        return
-    end
+if original_number > 1 %only allows program to continue if user entered a value greater than 1. Keyword value.
 
-    factors = zeros(1, 100); %prime factors of n
-    i = 1; %holds next location to insert into factors
-    divisor = 2; %divisor to check n against
-    start = 3; %start of the check interval
-    stop = ceil(n/2); %end of the check interval
+   sum = 0;
 
-    %handle checking 2s
-    while rem(n, divisor) == 0
-        n = n / divisor;
-        factors(i) = divisor;
-        i = i + 1;
-    end
+   for i = 1:12 %iterates through the array of prime numbers
     
-    %handle rest needed checks
-    for divisor = start:2:stop
-        while rem(n, divisor) == 0
-            n = n / divisor;
-            factors(i) = divisor;
-            i = i + 1;
-        end
-    end
-    
-    factors = factors(1:i-1); %handle the 
-    
+    %checks the number for each prime, until that specific prime is no longer divisible
+       while mod(number_being_tested,list_of_prime_numbers(i)) == 0 
+           if original_number == list_of_prime_numbers(i)
+               break;
+           end
+           
+           number_being_tested = number_being_tested/list_of_prime_numbers(i);
+   
+           if sum == 0
+               fprintf('%d = %d', original_number, list_of_prime_numbers(i));
+     
+           else
+               fprintf(' * %d', list_of_prime_numbers(i)); 
+           end
+           sum = sum+ 1;
+       end
+       
+       if number_being_tested == 1
+       break;
+       end
+   end
+
+   
+   if(sum==0)
+       fprintf('It appears the number you entered is prime.');
+   elseif number_being_tested ~=1
+       fprintf(' * %d', number_being_tested);
+   end
+   fprintf('\n');
+else %does not run program if value is less than or equal to one, keyword value
+ fprintf('You have made an impropper input, please enter a positive number greater than 1');
 end
